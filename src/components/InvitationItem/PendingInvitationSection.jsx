68f6809f09b7a9378c5ef227436f35b6e8d60978
natasha1013 <113877220+natasha1013@users.noreply.github.com>
2025-06-04 18:19:23 +0800

    added invitation, tested the invitation success

---
+++ src/components/InvitationItem/PendingInvitationSection.jsx
@@ -0,0 +1,8 @@
+export default function PendingInvitationSection() {
+  return (
+    <div className="mt-6">
+      <p>This is the Pending Invitation section content.</p>
+      {/* Add pending list here */}
+    </div>
+  );
+}
